b6c37fcb2d10d6f349f152259a5eda92b29a31e7
Priyanuj Bora <while.alive.program@gmail.com>
2022-12-25 19:22:48 +0530

    Format for integer output

---
+++ Basics/tests/FormatInt2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int a, b;
+
+	printf("enter values of a and b: ");
+
+	scanf("%d%d", &a, &b);
+
+
+	printf("a=%3d\nb=%4d\n", a, b);
+
+	return 0;
+}
