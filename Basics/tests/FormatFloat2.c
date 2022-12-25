eafe3a72765bad062fdc0e76113c5db82005fe02
Priyanuj Bora <while.alive.program@gmail.com>
2022-12-25 20:36:34 +0530

    Format for float output

---
+++ Basics/tests/FormatFloat2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+	float a, b;
+
+	printf("enter values of a and b: ");
+	scanf("%f%f", &a, &b);
+
+	printf("\na=%4.1f\nb=%7.2f\n", a, b);
+
+	return 0;
+}
