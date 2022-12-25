985d2792ce5474645fd401992029e87d7af211af
Priyanuj Bora <while.alive.program@gmail.com>
2022-12-25 20:22:52 +0530

    Format for float input

---
+++ Basics/tests/FormatFloat1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	float a, b;
+
+	printf("enter values of a and b: ");
+
+	scanf("%3f%4f", &a, &b);
+
+	printf("\na=%f\nb=%f\n", a, b);
+	return 0;
+}
