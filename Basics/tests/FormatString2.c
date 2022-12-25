8a3f122c07793ee5dab32cdde936b1e8e1bd6a16
Priyanuj Bora <while.alive.program@gmail.com>
2022-12-25 22:59:40 +0530

    Format for string output

---
+++ Basics/tests/FormatString2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("%3s\n", "Priyanuj");
+
+	printf("%10s\n", "Mouse");
+
+	printf("%.3s\n", "Priyanuj");
+
+	printf("%8.3s\n", "Priyanuj");
+
+	return 0;
+}
