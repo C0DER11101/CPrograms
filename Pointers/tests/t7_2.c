992d92d140fbcd44489f6ac71bf29b7bea8e07a7
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-06 12:29:47 +0530

    Void pointer arithmetic

---
+++ Pointers/tests/t7_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int a[]={1, 2, 3, 4, 5};
+	void *vptr;
+
+	vptr=a; // vptr=&a[0];
+
+	for(int i=0; i<5; i++)
+		printf("%d\t", *(int*)vptr+i);
+
+	printf("\n");
+	return 0;
+}
