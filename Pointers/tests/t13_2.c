942904e79d0634d35e733a79bc638cdf53a2bb4f
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-07 22:42:29 +0530

    Function's address sent as argument

---
+++ Pointers/tests/t13_2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(int, int);
+int sum(int, int, int(*)(int, int));
+int main(void)
+{
+	int a, b;
+
+	printf("enter value of a and b: ");
+	scanf("%d%d", &a, &b);
+	int result;
+
+	result=sum(a, b, add);
+
+	printf("\n%d+%d=%d\n", a, b, result);
+
+	return 0;
+}
+
+int add(int a, int b)
+{
+	return (a+b);
+}
+
+int sum(int a, int b, int(*addNums)(int, int))
+{
+	int result=(*addNums)(a, b);
+	return result;
+}
