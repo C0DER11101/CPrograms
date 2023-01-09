f2f11b98d047229dea76bdcc72671c37f90c5a5a
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-09 13:22:05 +0530

    Subscripting string constant

---
+++ Strings/tests/t2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("%c\n", "software"[0]);
+	printf("%c\n", "software"[1]);
+	printf("%c\n", "software"[2]);
+	printf("%c\n", "software"[3]);
+	printf("%c\n", "software"[4]);
+	return 0;
+}
