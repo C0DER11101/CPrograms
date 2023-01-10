ab494cb4b4d60e50420e9ebb00089a0574856668
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-10 10:51:56 +0530

    usage of strlen()

---
+++ Strings/tests/t6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include<string.h>
+
+int main(void)
+{
+	char *p="Hello";
+
+	printf("\nLength of the string is: %lu\n", strlen(p));
+	return 0;
+}
