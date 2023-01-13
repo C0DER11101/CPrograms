a789672f05d2fa59fa2c951b8e03df11659ff8a1
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-13 19:14:09 +0530

    sprintf()

---
+++ Strings/tests/t24.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	char *str;
+
+	str=(char*)malloc(30*sizeof(char));
+
+	int x=450;
+
+	sprintf(str, "%d", x);
+
+	printf("\nstr=%s\n", str);
+
+	free(str);
+
+	return 0;
+}
