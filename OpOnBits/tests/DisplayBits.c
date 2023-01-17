29d104d20bd51875bd576004810cc7d65a2c805c
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-17 16:33:02 +0530

    Function for displaying 32-bit binary equivalent of decimal and hexadecimal numbers

---
+++ OpOnBits/tests/DisplayBits.c
@@ -0,0 +1,19 @@
+int bits[32]; // 32 bits
+
+void DisplayBits(int dec)
+{
+	int i=31;
+	while(dec!=0 && i>=0)
+	{
+		bits[i--]=dec%2;
+		dec/=2;
+	}
+
+	for(int j=0; j<32; j++)
+	{
+		printf("%d", bits[j]);
+		if(j%8==7)
+			printf(" ");
+	}
+	return;
+}
