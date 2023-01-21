cd92b7a639dc1567c7ae84b86bad215dfbe09365
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-21 22:55:59 +0530

    Problem

---
+++ CPreprocessors/tests/t13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SWAP(dtype, x, y) {dtype t; t=x, x=y, y=t;}
+
+int main(void)
+{
+	int s=2, t=4;
+
+	printf("\nBefore swapping\n");
+	printf("s=%d\tt=%d\n", s, t);
+
+	SWAP(int, s, t); // {int t; t=s, s=t, t=s;}
+
+	printf("\nAfter swapping\n");
+	printf("s=%d\tt=%d\n", s, t);
+
+	return 0;
+}
