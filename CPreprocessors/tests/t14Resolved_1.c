55c0f12c92c616aa4e944f6eddebdc3ac4605072
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-22 12:01:56 +0530

    1st way to resolve problem of t14.c

---
+++ CPreprocessors/tests/t14Resolved_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define ZERORUN(x) if(x==0) printf("\n0 runs!!\n")
+
+
+int main(void)
+{
+	int runs=12;
+
+	if(runs<100)
+	{
+		printf("Runs are less than a century!\n");
+		ZERORUN(runs);
+	}
+
+	else
+		printf("Scored over a century\n");
+
+	return 0;
+}
