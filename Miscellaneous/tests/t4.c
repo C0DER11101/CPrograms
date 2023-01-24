ba0d25e87a955974d882f7c33660fee51289e1a5
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-24 13:22:54 +0530

    ERROR

---
+++ Miscellaneous/tests/t4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+enum vowels { a, e, i, o, u};
+
+int main(void)
+{
+	enum vowels v1=b, v2=i;
+
+	printf("\nValue of i = %d\n", v2);
+	printf("\nValue of b = %d\n", v1);
+	return 0;
+}
