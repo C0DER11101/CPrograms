0f5268ba8397dd9e2af626da724e404e347e4d7c
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-24 12:38:44 +0530

    Modifying enumerators in enumeration list

---
+++ Miscellaneous/tests/t2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+enum vowels
+{
+	a=2, e, i, o, u
+};
+
+int main(void)
+{
+	printf("%d\n", a);
+	printf("%d\n", e);
+	printf("%d\n", i);
+	printf("%d\n", o);
+	printf("%d\n", u);
+
+	return 0;
+}
