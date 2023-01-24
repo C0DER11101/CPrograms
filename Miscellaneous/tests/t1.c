4a1a0473459e65ed2dbac681aa64e95fe6690f19
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-24 12:29:02 +0530

    Miscellaneous

---
+++ Miscellaneous/tests/t1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+enum month
+{
+	Jan,
+	Feb,
+	Mar,
+	Apr,
+	May,
+	Jun,
+	Jul,
+	Aug,
+	Sep,
+	Oct,
+	Nov,
+	Dec
+};
+
+int main(void)
+{
+	printf("%d\n", Jan);
+	printf("%d\n", Feb);
+	printf("%d\n", Mar);
+	return 0;
+}
