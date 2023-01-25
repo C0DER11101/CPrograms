9f894a8079f9b184eacc4789bb008ae2c1b0da9c
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-25 13:07:59 +0530

    ERROR

---
+++ Miscellaneous/tests/t10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int a=5, b=6;
+
+	int* const p=&a;
+
+	printf("a = %d\n", *p);
+	*p=10; // valid!!
+	printf("a = %d\n", *p);
+
+	p=&b; // invalid
+	printf("b = %d\n", *p);
+
+	return 0;
+}
