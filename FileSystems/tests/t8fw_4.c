6687111b53a6293b64341a20483d0e9ee6ae2558
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-31 08:17:02 +0530

    Writing structure to a file using fwrite()

---
+++ FileSystems/tests/t8fw_4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct student
+{
+	char name[20];
+	unsigned int age;
+};
+
+int main(void)
+{
+	struct student record={"noname", 0};
+
+	FILE* fptr;
+
+	if((fptr=fopen("Test9.txt", "wb"))==NULL)
+	{
+		printf("\nFailed to open file!!\n");
+		return 0;
+	}
+
+	printf("\nFile opened successfully!!\n");
+
+	printf("\nenter details of the students:\n");
+
+	for(int i=0; i<2; i++)
+	{
+		printf("\nStudent %d\n", i+1);
+		printf("Name: ");
+		scanf("%s", record.name);
+		printf("Age: ");
+		scanf("%d", &record.age);
+
+		if(fwrite(&record, sizeof(sizeof(record)), 1, fptr)==1)
+			printf("\nDetails written successfully!!\n");
+
+		else
+			printf("\nFailed to write!!\n");
+	}
+
+	fclose(fptr);
+
+	return 0;
+}
