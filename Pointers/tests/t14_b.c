1b8872751ea54cfe632b8464c4aba204f1580bb0
Priyanuj Bora <while.alive.program@gmail.com>
2023-02-19 22:39:16 +0530

    Type conversion without void pointer

---
+++ Pointers/tests/t14_b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int a=10, *p=&a;
+
+
+	printf("a = %d\n", *p);
+
+	float b=10.0f;
+
+	p=(int*)&b;
+
+	printf("b = %f\n", (float)*p);
+
+	return 0;
+}
